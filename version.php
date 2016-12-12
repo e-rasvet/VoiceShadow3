48d11ddd8fbf6864963d408b9e6cb96c89975fa9
Igor Nikulin <dyhanie.istiny@gmail.com>
2016-12-12 19:31:29 +0500

    First commit
    Original files

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php // $Id: version.php,v 1.2 2012/03/10 22:00:00 Igor Nikulin Exp $
+
+$plugin->version  = 2016102200;
+$plugin->requires = 2015051100;
+$plugin->release = '3.0.0.1';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->cron     = 180;
+$plugin->component = 'mod_voiceshadow';
